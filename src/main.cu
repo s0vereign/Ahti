59443d0c4495fd7b2dd322c5ce0c233bdc9e3b09
s0vereign <maxboehme@live.de>
2016-12-01 15:18:17 +0100

    Defined Local and Globalgrid

--- src/main.cu
+++ src/main.cu
@@ -1,14 +1,21 @@
-#include <iostream>
-#include <alpaka/alpaka.hpp>
 
+#include <iostream>
+
+// GRAYBAT
+#include <graybat/Cage.hpp>
+#include <graybat/communicationPolicy/BMPI.hpp>
+#include <graybat/graphPolicy/BGL.hpp>
+
+
+#include <alpaka/alpaka.hpp>
+#include <mpi.h>
+
+#include "grid/Globalgrid.h"
+#include "grid/Localgrid.h"
 
 int
 main()
 {
-	using std::cout;
-	using std::endl;
-	
-	cout << "Done!" << endl;
 	
 	return 0;
 }
