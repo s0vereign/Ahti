6a703aa57701ffe2fc1c1b67ca5359bed28b941d
s0vereign <Maximilian.Boehme@mailbox.tu-dresden.de>
2017-01-08 00:48:05 +0100

    Kicked Graybat from dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,26 @@
 #include <iostream>
 #include <vector>
 
-// GRAYBAT
-#include <graybat/Cage.hpp>
-#include <graybat/communicationPolicy/ZMQ.hpp>
-#include <graybat/graphPolicy/BGL.hpp>
-#include <graybat/pattern/GridDiagonal.hpp>
-#include <graybat/mapping/Consecutive.hpp>
-#include <graybat/pattern/HyperCube.hpp>
-
 // Alpaka for Kernel Acceleration
 #include <alpaka/alpaka.hpp>
+
+// MPI
+#include <mpi.h>
 
 #include "grid/Globalgrid.h"
 #include "grid/Localgrid.h"
 
-auto
-main()
-->int
+int
+main(int argc, char **argv)
 {
-	typedef graybat::communicationPolicy::ZMQ   CommunicationPolicy;
-	typedef typename CommunicationPolicy::Config Config;
-	typedef graybat::graphPolicy::BGL <> GraphPolicy;
-	typedef graybat::Cage<CommunicationPolicy, GraphPolicy> Cage;
-	Config config;
-	Cage cage(config); 
-	cage.setGraph(graybat::pattern::GridDiagonal<GraphPolicy>(4,1));
-	cage.distribute(graybat::mapping::Consecutive());
-	typedef Cage::Vertex Vertex;
-	// Iterate over all vertices that I host
-	typedef Cage::Edge Edge;
-	for(Vertex vertex: cage.getHostedVertices()){
-    for(Edge outEdge : cage.getOutEdges(vertex)){
-        Vertex sourceVertex = outEdge.source;
-        Vertex targetVertex = outEdge.target;
-        std::cout << "From vertex " << sourceVertex.id
-                  << " over edge "   << outEdge.id
-                  << " to vertex "   << targetVertex.id << std::endl;
-    }
-	}
+	MPI_Init(&argc, &argv);
 	
-
-	
+	int size;
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	std::cout << size << " " << rank << std::endl;
+	MPI_Finalize();
 	
 	return EXIT_SUCCESS;
 }
