6aaf87f6dc6db9e97e4c8e75139e26ad46b90818
s0vereign <maxboehme@live.de>
2017-03-20 14:27:11 +0100

    finished Coeff evolve logic @TOTEST

--- include/math/ValsEv.hpp
+++ include/math/ValsEv.hpp
@@ -17,6 +17,11 @@ namespace math{
     using std::exp;
     using Grid::Localgrid1D;
 
+    int get_n(int n, int gnx)
+    {
+        return n - gnx/2;
+    }
+
     template<typename T_CONF>
     void vals_ev(vector<complex<double> >& vals,
                  vector<complex<double> >& coeff,
@@ -25,6 +30,18 @@ namespace math{
                  int size, 
                  int rank)
     {
-        
+        const int nx = lgrix.nx;
+        const int gnx = T_CONF.gnx;
+        const double l0 = lgrid.xmax - lgrid.xmin;
+        complex<double> iu(0,1);
+
+        for(int i = 0; i < nx; i++)
+        {
+            for(int j = 0; i < nx; j++)
+            {
+                int n = getn(j, gnx);
+                vals[i] += coeff[j]*exp(iu*2*M_PI/l0*n)
+            }
+        }
     }
 }
