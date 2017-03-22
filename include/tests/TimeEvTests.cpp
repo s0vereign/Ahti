89446c7cc450cab7c531fde252d1e975bbb5142b
s0vereign <maxboehme@live.de>
2017-03-22 11:28:30 +0100

    Added file for Timeevolution tests.
    TODO:
    - Think about test cases
    - Write tests for coef_ev and vals_ev
    - If succeded put all functions together in Worker::ev_step

---
+++ include/tests/TimeEvTests.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <complex>
+#include <vector>
+
+#include "math/CoeffEv.hpp"
+#include "math/ValsEv.hpp"
+
+#define PI 3.141592653589793
+
+#define BOOST_TEST_MODULE "FourierTest"
+
+#include "boost/test/included/unit_test.hpp"
+
+
+
+
