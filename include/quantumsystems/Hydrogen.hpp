7d3f3c4f105e0245f0eec6c2127f51df69c0285d
s0vereign <maximilian.boehme@mailbox.tu-dresden.de>
2018-01-19 12:55:10 +0100

    Fixed horribly wrong notation

--- include/quantumsystems/Hydrogen.hpp
+++ include/quantumsystems/Hydrogen.hpp
@@ -12,22 +12,44 @@ namespace qsystems
         class Hplus_Pot
         {
         public:
-            const std::complex<double> operator(const std::complex<double>& x,
+            const std::complex<double> operator()(const std::complex<double>& x,
                                                 const std::complex<double>& y,
                                                 const std::complex<double>& z);
             Hplus_Pot() = default;
 
         };
 
+        struct Y_lm
+        {
+        public:
+            Y_lm() = default;
+            const std::complex<double>
+                    operator()( int l, int m,
+                                const std::complex<double>& x,
+                                const std::complex<double>& y,
+                                const std::complex<double>& z);
+        };
+
+        struct R_nl
+        {
+        public:
+            R_nl() = default;
+            const std::complex<double> operator()( int n, int l,
+                                                const std::complex<double>& x_,
+                                                const std::complex<double>& y_,
+                                                const std::complex<double>& z_);
+        };
+
         struct Psi_nlm
         {
-            std::complex<double> operator(int n, int l, int m,
+        public:
+
+            std::complex<double> operator()(  int n, int l, int m,
                                             const std::complex<double>& x,
                                             const std::complex<double>& y,
-                                            const std::complex<double>& z)
-            {
-
-            }
+                                            const std::complex<double>& z);
         };
+
+
     }
 }
--- src/quantumsystems/Hydrogen.cpp
+++ src/quantumsystems/Hydrogen.cpp
@@ -1,9 +1,31 @@
-#include "../../include/quantumsystems/Hydrogen.hpp"
-#include <cmath>
 
-const std::complex<double> qsystems::hydrogen::Hplus_Pot::operator(const std::complex<double>& x,
+#include <boost/math/special_functions/laguerre.hpp>
+#include <cmath>
+#include "../../include/quantumsystems/Hydrogen.hpp"
+#include "../../include/math/Factorial.hpp"
+#include "../../include/math/CartToSpherical.hpp"
+const std::complex<double> qsystems::hydrogen::Hplus_Pot::operator()(const std::complex<double>& x,
                                                                    const std::complex<double>& y,
                                                                    const std::complex<double>& z)
 {
-    return - 1 / sqrt(x.real()*x.real() + y.real() * y.real() + z.real() * z.real());
-}
+    return - 1.0 / sqrt(x.real()*x.real() + y.real() * y.real() + z.real() * z.real());
+};
+
+const std::complex<double> qsystems::hydrogen::R_nl::operator()( int n, int l,
+                                                                const std::complex<double>& x_,
+                                                                const std::complex<double>& y_,
+                                                                const std::complex<double>& z_)
+{
+    double x = x_.real();
+    double y = y_.real();
+    double z = z_.real();
+
+
+    double r = sqrt(x*x+y*y+z*z);
+    double pref = sqrt(pow(2/n,3)*math::fac(n-l-1)/(2 * n * math::fac(n +l)));
+    double roh = 2.0*r/n;
+    double L = boost::math::laguerre(unsigned(n-l-1), unsigned(2*l+1), roh);
+
+    return pref*std::exp(-roh/2.0)*pow(roh,l)*L;
+
+};
